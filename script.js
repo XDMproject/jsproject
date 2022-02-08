a455fff5313ae24c82d8d2bc83d62b874134d699
XDM <vsdm1994@inbox.ru>
2022-02-08 16:41:38 +1000

    Lesson1

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 "use strict"; //позволяет избавиться от неточностей
 
+const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
+// personalMovieDB.movies = prompt("Один из последних просмотренных Фильмов?");
+
+let personalMovieDB = {
+
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+
+};
+
+  const a = prompt("Просмотренный фильм", ""),
+        b = prompt("Рейтинг фильма", ""),
+        c = prompt("Просмотренный фильм", ""),
+        d = prompt("Рейтинг фильма", "");
+
+
+    personalMovieDB.movies[a] = b;
+    personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
